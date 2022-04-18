2c66c823a65b1188537befa6442c925b45b51bb0
Rafikhan15 <mdrafi.khan0089@gmail.com>
2022-04-18 19:53:17 +0600

    header and footer component added

--- src/App.js
+++ src/App.js
@@ -16,21 +16,7 @@ function App() {
   return (
     <div>
       <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
-        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
-        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/register" element={<Register></Register>}></Route>
-        <Route path="/checkout" element={
-          <RequireAuth>
-            <Checkout></Checkout>
-          </RequireAuth>
-        }></Route>
-        <Route path="*" element={<NotFound></NotFound>}></Route>
-      </Routes>
+
       <Footer></Footer>
     </div>
   );
