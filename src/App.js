bf05117804a265b78b367979a37a4223955d5e14
Rafikhan15 <mdrafi.khan0089@gmail.com>
2022-04-18 20:35:21 +0600

    Require auth component added

--- src/App.js
+++ src/App.js
@@ -23,7 +23,12 @@ function App() {
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
-
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/checkout" element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
 
       </Routes>
