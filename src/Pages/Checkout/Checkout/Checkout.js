a6cc61ff55463b76f90775ebf756573b1e57c096
Rafikhan15 <mdrafi.khan0089@gmail.com>
2022-04-18 20:59:11 +0600

    serviceDetail compontes added

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 const Checkout = () => {
     return (
         <div>
             <h2>Please Checkout your booking</h2>
+            <Row>
+                <Col>
+                    <img src="" alt="" />
+                </Col>
+                <Col>
+                </Col>
+
+            </Row>
+
         </div>
+
+
     );
 };
 
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
     return (
-        <div>
-            <h2>Welcome to detail: {serviceId}</h2>
-            <div className='text-center'>
-                <Link to="/checkout">
-                    <button className='btn btn-primary'>Proceed Checkout</button>
-                </Link>
-            </div>
+        <div className='container'>
+            <h2 className='text-center'>Welcome to detail: {serviceId}</h2>
+
+            <Row>
+                <Col>
+
+                    <img src="https://i.ibb.co/cXSMccq/ban1.jpg" alt="" />
+                </Col>
+                <Col>
+                    <div className='text-center'>
+                        <Link to="/checkout">
+                            <button className='btn btn-primary'>Proceed Checkout</button>
+                        </Link>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
